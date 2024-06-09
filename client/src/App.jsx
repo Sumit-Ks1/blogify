71a3b4f82d5d27a45f9a45df6788a8013091c2db
Sumit-Ks1 <sumit127624@gmail.com>
2024-06-09 11:55:14 +0530

    cretaed routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About" 
+import Signin from "./pages/Signin"
+import Signup from "./pages/Signup"
+import Projects from "./pages/Projects"
+import Dashboard from "./pages/Dashboard"
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/projects" element={<Projects/>} />
+        <Route path="/sign-in" element={<Signin/>} />
+        <Route path="/sign-up" element={<Signup/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
